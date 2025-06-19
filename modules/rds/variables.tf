b79c459d0fc63a8fca36b85bb37ce5230de7c3b3
B. Arun Sreekanth <141525982+arun-sreekanth@users.noreply.github.com>
2025-06-19 17:04:46 -0400

    Create variables.tf

---
+++ modules/rds/variables.tf
@@ -0,0 +1,27 @@
+variable "allocated_storage" {
+  type = number
+}
+
+variable "engine" {
+  type = string
+}
+
+variable "instance_class" {
+  type = string
+}
+
+variable "db_name" {
+  type = string
+}
+
+variable "username" {
+  type = string
+}
+
+variable "password" {
+  type = string
+}
+
+variable "name" {
+  type = string
+}
